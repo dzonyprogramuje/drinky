ed439b25723bf0b4fb3fd3af92f80e941df66d28
dzonyprogramuje <dzonyprogramuje@gmail.com>
2022-09-06 18:32:51 +0200

    Start Screen basic implemented

--- src/screens/StartScreen.js
+++ src/screens/StartScreen.js
@@ -1,13 +1,21 @@
 import React from "react";
 import MainButtonComponent from "../components/MainButtonComponent";
 import "./StartScreen.scss";
+import AlkoholImage from "../images/alkohol.svg";
 
 export default function StartScreen() {
   return (
     <div className="start-screen-container">
-      <h1>Drinky</h1>
-      <h2>Pijacka gra alkoholowa</h2>
-      <MainButtonComponent>Start</MainButtonComponent>
+      <img
+        src={AlkoholImage}
+        alt="Drinky - Pijacka Gra Alkoholowa - Start Screen"
+        className="alkohol"
+      />
+      <div className="start-screen-text-container">
+        <h1>Drinky</h1>
+        <h2>Pijacka gra alkoholowa</h2>
+        <MainButtonComponent>Start</MainButtonComponent>
+      </div>
     </div>
   );
 }
