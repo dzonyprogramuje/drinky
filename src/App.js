2ea602dffde7932dd9bbb76e6ebdffc6b38a3b4a
dzonyprogramuje <dzonyprogramuje@gmail.com>
2022-09-06 17:51:06 +0200

    add react router dom ^5 (version 6 does not work)

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import "./App.scss";
 import StartScreen from "./screens/StartScreen";
 
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
 function App() {
   return (
     <div className="main-container">
-      <StartScreen />
+      <Router>
+        <Switch>
+          <Route path="/" exact>
+            <StartScreen />
+          </Route>
+          {/* <Route path="/game" exact>
+            <GameScreen />
+          </Route>
+          <Route path="/over">
+            <OverScreen />
+          </Route> */}
+        </Switch>
+      </Router>
     </div>
   );
 }
