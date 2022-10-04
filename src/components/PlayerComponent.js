04f1c167ef3e50b941bfbf46a133675b74c25242
dzonyprogramuje <dzonyprogramuje@gmail.com>
2022-10-04 18:46:41 +0200

    add some animation on player add

--- src/components/PlayerComponent.js
+++ src/components/PlayerComponent.js
@@ -13,6 +13,44 @@ const StyledPlayer = styled.div`
   background-color: var(--color-light);
   border-radius: var(--round-base);
 
+  /* animation: append-animate 0.3s linear; */
+
+  /* @keyframes append-animate {
+    from {
+      transform: scale(0);
+      opacity: 0;
+    }
+    to {
+      transform: scale(1);
+      opacity: 1;
+    }
+  } */
+
+  -webkit-animation: scale-up-center 0.4s cubic-bezier(0.39, 0.575, 0.565, 1)
+    both;
+  animation: scale-up-center 0.4s cubic-bezier(0.39, 0.575, 0.565, 1) both;
+
+  @-webkit-keyframes scale-up-center {
+    0% {
+      -webkit-transform: scale(0.5);
+      transform: scale(0.5);
+    }
+    100% {
+      -webkit-transform: scale(1);
+      transform: scale(1);
+    }
+  }
+  @keyframes scale-up-center {
+    0% {
+      -webkit-transform: scale(0.5);
+      transform: scale(0.5);
+    }
+    100% {
+      -webkit-transform: scale(1);
+      transform: scale(1);
+    }
+  }
+
   &:hover,
   &:active {
     border: 1px solid var(--color-base);
